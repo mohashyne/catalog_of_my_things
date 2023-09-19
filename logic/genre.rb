74f2d01093e733f8ef4e33ebe9f2b3cd8e4505f5
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-09-19 12:03:36 -0600

    Fix linters

--- logic/genre.rb
+++ logic/genre.rb
@@ -1,15 +1,14 @@
 class Genre
-    attr_accessor :id, :name, :items
-  
-    def initialize(id, name)
-      @id = id
-      @name = name
-      @items = []
-    end
-  
-    def add_item(item)
-      @items.push(item)
-      item.genre = self
-    end
+  attr_accessor :id, :name, :items
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
   end
-  
+
+  def add_item(item)
+    @items.push(item)
+    item.genre = self
+  end
+end
